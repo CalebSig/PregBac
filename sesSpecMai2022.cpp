99a8ebc40cddcd2109ad496fc4628b2c379610e2
CalebSig <Sighiartau.Do.Achim@student.utcluj.ro>
2025-06-01 16:39:36 +0300

    Update sesSpecMai2022.cpp

--- sesSpecMai2022.cpp
+++ sesSpecMai2022.cpp
@@ -1,3 +1,21 @@
+/*
+
+Numim secvență progresivă a unui șir crescător de numere naturale un subșir al acestuia, format din termeni aflați pe poziții consecutive în șirul dat, cu proprietatea că fiecare termen apare în subșir de un număr de ori egal cu valoarea sa. Lungimea secvenței este egală cu numărul de termeni ai acesteia.
+
+Fișierul bac.txt conține un șir crescător de cel mult 
+ numere naturale din intervalul 
+, astfel încât orice termen al șirului apare de un număr de ori cel mult egal cu valoarea sa. Numerele sunt separate prin câte un spațiu.
+
+Se cere să se afișeze pe ecran lungimea maximă a unei secvențe progresive din șirul aflat în fișier.
+
+Proiectați un algoritm eficient din punctul de vedere al timpului de executare și al memoriei utilizate.
+
+Exemplu: dacă fişierul conţine numerele 1 2 2 3 4 4 4 4 6 6 6 6 6 6 7 7 7 8 8 8 8 8 8 8 8 atunci pe ecran se afișează valoarea 10.
+
+    */
+
+
+
 #include <iostream>
 #include <fstream>
 using namespace std;
